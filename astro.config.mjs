83af2b36635db4d12435b060155ef1abc30e0845
MonadWizard <monad.wizard.r@gmail.com>
2026-06-27 12:44:33 +0600

    blog deploy 0.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.rakib.click',
+  site: 'https://rakib.click',
   base: '/',
   devToolbar: { enabled: false },
   integrations: [mdx(), sitemap()],
